e9f762e2220ebaf21afc0155e89a46beebf8609a
elnur833 <58503297+elnur833@users.noreply.github.com>
2024-11-17 13:42:14 +0400

    blogger home adjustments

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,3 +45,27 @@ function copyTag(button) {
 $('.show_example').click(function(){
     $('.tag-exampple-image').fadeToggle();
 })
+
+// Format options
+var optionFormat = function(item) {
+    if ( !item.id ) {
+        return item.text;
+    }
+
+    var span = document.createElement('span');
+    var imgUrl = item.element.getAttribute('data-kt-select2-user');
+    var template = '';
+
+    template += '<img src="' + imgUrl + '" class="rounded-circle h-20px me-2" alt="image"/>';
+    template += item.text;
+
+    span.innerHTML = template;
+
+    return $(span);
+}
+
+// Init Select2 --- more info: https://select2.org/
+$('.account-selector-select').select2({
+    templateSelection: optionFormat,
+    templateResult: optionFormat
+});
